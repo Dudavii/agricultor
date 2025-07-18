56d6d3c503a4fba7f5b3bbf52a29a96752a0191e
Dudavii <vitreduarda@gmail.com>
2025-07-18 14:55:41 +0000

    começo_acricultura

---
+++ projeto.py
@@ -0,0 +1,30 @@
+
+
+
+c_morango ={'arr':5.0, 'mudas':100,'insumos':30, 'maobra':60, 'energia': 600}
+b_morango =[8.000,500]
+
+c_abacaxi ={'arr':6.0, 'mudas':150,'insumos':31, 'maobra':50, 'energia':700}
+b_abacaxi =[3.000,510]
+
+
+c_batata ={'arr':9.0, 'mudas':180,'insumos':50, 'maobra':70, 'energia':400}
+b_batata =[4.000,600]
+
+
+c_abobora ={'arr':5.0, 'mudas':60,'insumos':80, 'maobra':40, 'energia':300}
+b_abobora =[5.000,200]
+
+
+c_manga ={'arr':7.0, 'mudas':80,'insumos':100, 'maobra':75, 'energia':500}
+b_manga =[2.000,200]
+
+quan_hec =int(input("Digite a quantidade de hectares: "))
+produto =input(" Digite o produto:")
+
+morango = 1
+abacaxi = 2
+batata = 3
+abobora = 4
+manga = 5 
+
